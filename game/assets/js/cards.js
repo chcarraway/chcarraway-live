6fc3a5d3eb8d27ff73551249f3f4584abe3693f8
Corey Carraway <chcarraway@gmail.com>
2019-10-08 17:37:00 -0400

    drinking game functioning

--- game/assets/js/cards.js
+++ game/assets/js/cards.js
@@ -1,8 +1,7 @@
-var card = {
-    card000000: {
-        text: "Everybody drink!"
-    },
-    card000001: {
-        text: "Player to your left drinks!"
-    }
-}
+var cards = [
+    "Everybody drink!",
+    "Player to your left drinks!",
+    "Player to your right drinks!",
+    "If you don't like disco music, drink!",
+    "The last player to enter the room drinks!"
+]
--- game/assets/js/code.js
+++ game/assets/js/code.js
@@ -1,10 +1,33 @@
-function showCard() {
-    var i = 0;
-    var newCard = Object.keys(card);
-    document.getElementById("card").innerHTML = newCard.text;
-    var i = i++;
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
 }
 
-$(function(){
-    document.getElementById("nextCard").addEventListener("click", showCard);
-})
+var i = 0;
+cards = shuffle(cards);
+var card = Object.keys(cards);
+
+function showCard() {
+    if (i === card.length) {
+        i = 0;
+        alert("No new cards to display. Shuffling deck and starting over.")
+        document.getElementById('card').innerHTML = "Click the button below to begin";
+        cards = shuffle(cards);
+    } else {
+        var newCard = cards[card[i]];
+        document.getElementById('card').innerHTML = newCard;
+        i++;
+    };
+};
+
+document.getElementById("nextCard").addEventListener('click', showCard);
