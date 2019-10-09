382d1ffc5d2303993fd017b53e27c173732e883f
Corey Carraway <chcarraway@gmail.com>
2019-10-08 23:15:32 -0400

    Updated CSS for drinking game

--- game/assets/js/cards.js
+++ game/assets/js/cards.js
@@ -5,5 +5,5 @@ var cards = [
     "If you don't like disco music, drink!",
     "The last player to enter the room drinks!",
     "Choose a player to have a staring contest with. Loser drinks!",
-    "Avalanche! Choose a direction, and everyone starts drinking. the player beside you in that direction can't stop until you do, that player's neighbor can't stop until they do, etc..."
+    "Avalanche! Choose a direction, and everyone starts drinking. The player beside you in that direction can't stop until you do, that player's neighbor can't stop until they do, etc..."
 ]
