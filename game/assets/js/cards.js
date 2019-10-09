c9c7563f770b738b08b50746d7eee3b7504afec0
Corey Carraway <chcarraway@gmail.com>
2019-10-08 20:41:39 -0400

    added cards to drinking game

--- game/assets/js/cards.js
+++ game/assets/js/cards.js
@@ -3,5 +3,7 @@ var cards = [
     "Player to your left drinks!",
     "Player to your right drinks!",
     "If you don't like disco music, drink!",
-    "The last player to enter the room drinks!"
+    "The last player to enter the room drinks!",
+    "Choose a player to have a staring contest with. Loser drinks!",
+    "Avalanche! Choose a direction, and everyone starts drinking. the player beside you in that direction can't stop until you do, that player's neighbor can't stop until they do, etc..."
 ]
