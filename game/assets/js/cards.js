2147987df3dd175f7d1c0973174e9bfc7a733238
root <root@localhost>
2019-10-09 22:28:25 -0400

    added more cards

--- game/assets/js/cards.js
+++ game/assets/js/cards.js
@@ -1,27 +1,27 @@
 var cards = [
     {
-        header: "All together now!",
+        header: "All Together Now!",
         body: "Everybody drink!"
     },{
-        header: "To the left!",
+        header: "To The Left!",
         body: "Player to your left drinks!"
     },{
-        header: "To the right!",
+        header: "To The Right!",
         body: "Player to your right drinks!"
     },{
-        header: "Boogie down?",
+        header: "Boogie Down?",
         body: "If you don't like disco music, drink!"
     },{
-        header: "If you're not first...",
+        header: "If You're Not First...",
         body: "The last player to enter the room drinks!"
     },{
-        header: "Look at me!",
+        header: "Look At Me!",
         body: "Choose a player to have a staring contest with. Loser drinks!"
     },{
         header: "Avalanche!",
         body: "Choose a direction, and everyone starts drinking. The player beside you in that direction can't stop until you do, that player's neighbor can't stop until they do, etc..."
     },{
-        header: "Nose goes!",
+        header: "Nose Goes!",
         body: "Everybody touches their nose. Last one to do so drinks!"
     },{
         header: "Rhyme Time",
@@ -33,25 +33,25 @@ var cards = [
         header: "Twinsies!",
         body: "All players with the same beverage as you must drink!"
     },{
-        header: "I'm the captain now",
+        header: "I'm The Captain Now",
         body: "Create a rule. If anyone breaks that rule, they drink!"
     },{
         header: "Never Have I Ever",
         body: "Play a round of Never Have I Ever"
     },{
-        header: "Mirror Mirror on the Wall",
+        header: "Mirror Mirror On The Wall",
         body: "Everyone vote on who has the best haircut. Winner drinks!"
     },{
-        header: "To the Window...",
+        header: "To The Window...",
         body: "Everybody must touch a wall. Last player to do so must drink!"
     },{
         header: "Lumberjack Fantasies",
         body: "Players with a beard must drink!"
     },{
-        header: "I Couldn't Come Up with a Witty Title",
+        header: "I Couldn't Come Up With A Witty Title",
         body: "Players without a beard must drink!"
     },{
-        header: "Baby Want a Bottle?",
+        header: "Baby Want A Bottle?",
         body: "The youngest player must drink!"
     },{
         header: "Get Off My Lawn!",
@@ -60,16 +60,16 @@ var cards = [
         header: "Maybe She's Born With It...",
         body: "All players wearing makeup must drink!"
     },{
-        header: "Third Time's a Charm",
+        header: "Third Time's A Charm",
         body: "Drink three times!"
     },{
         header: "Deuces Baby",
         body: "Drink two times!"
     },{
-        header: "I Couldn't Come Up with a Witty Title",
+        header: "I Couldn't Come Up With A Witty Title",
         body: "Take a drink!"
     },{
-        header: "Cats vs Dogs",
+        header: "Cats VS Dogs",
         body: "All players choose a side. Smallest group drinks!"
     },{
         header: "",
