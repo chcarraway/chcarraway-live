f83b9d3bd7afb7ce749570e897eafe5bc4c69f2e
Carraway <CarrawayC@lumosnet.com>
2019-10-10 19:27:02 -0400

    added ongoing effects table, supporting code

---
+++ game/assets/js/cards.test.js
@@ -0,0 +1,209 @@
+var cards = [
+    {
+        header: "Test Decay Three",
+        body: "decay: 3",
+        decay: 3
+    },{
+        header: "10",
+        body: ""
+    },{
+        header: "9",
+        body: "",
+        decay: 99
+    },{
+        header: "8",
+        body: "",
+        decay: 8
+    },{
+        header: "7",
+        body: ""
+    },{
+        header: "6",
+        body: ""
+    },{
+        header: "Test Decay Five",
+        body: "decay: 5",
+        decay: 5
+    },{
+        header: "4",
+        body: ""
+    },{
+        header: "3",
+        body: ""
+    },{
+        header: "2",
+        body: ""
+    },{
+        header: "1",
+        body: ""
+    },{
+        header: "0",
+        body: ""
+    }
+    
+/*
+    {
+        header: "To The Left!",
+        body: "Player to your left drinks!"
+    },{
+        header: "To The Right!",
+        body: "Player to your right drinks!"
+    },{
+        header: "Boogie Down?",
+        body: "If you don't like disco music, drink!"
+    },{
+        header: "If You're Not First...",
+        body: "The last player to enter the room drinks!"
+    },{
+        header: "Look At Me!",
+        body: "Choose a player to have a staring contest with. Loser drinks!"
+    },{
+        header: "Avalanche!",
+        body: "Choose a direction, and everyone starts drinking. The player beside you in that direction can't stop until you do, that player's neighbor can't stop until they do, etc..."
+    },{
+        header: "Nose Goes!",
+        body: "Everybody touches their nose. Last one to do so drinks!"
+    },{
+        header: "Rhyme Time",
+        body: "Say a word. The next player must say a word that rhymes with it. Go around the circle until someone fails. That player drinks!"
+    },{
+        header: "1, 2, 3, 4...",
+        body: "Choose a player to have a thumb war with. Loser drinks!"
+    },{
+        header: "Twinsies!",
+        body: "All players with the same beverage as you must drink!"
+    },{
+        header: "I'm The Captain Now",
+        body: "Create a rule. If any player breaks that rule, they drink!"
+    },{
+        header: "Never Have I Ever",
+        body: "Play a round of Never Have I Ever. All players must drink if they have done the thing!"
+    },{
+        header: "Mirror Mirror On The Wall",
+        body: "All players vote on who has the best haircut. Winner drinks!"
+    },{
+        header: "To The Window, To The Wall!",
+        body: "All players must touch a wall. Last player to do so must drink!"
+    },{
+        header: "Lumberjack Fantasies",
+        body: "All players with a beard must drink!"
+    },{
+        header: "I Couldn't Come Up With A Witty Title",
+        body: "All players without a beard must drink!"
+    },{
+        header: "Baby Want A Bottle?",
+        body: "The youngest player must drink!"
+    },{
+        header: "Get Off My Lawn!",
+        body: "The oldest player must drink!"
+    },{
+        header: "Maybe She's Born With It...",
+        body: "All players wearing makeup must drink!"
+    },{
+        header: "Third Time's A Charm",
+        body: "Drink three times!"
+    },{
+        header: "Deuces Baby",
+        body: "Drink two times!"
+    },{
+        header: "I Couldn't Come Up With A Witty Title",
+        body: "Take a drink!"
+    },{
+        header: "Cats VS Dogs",
+        body: "All players choose a side. Smallest group drinks!"
+    },{
+        header: "Coke VS Pepsi",
+        body: "All players choose a side. Smallest group drinks!"
+    },{
+        header: "This One Time, At Band Camp...",
+        body: "If you've ever played a musical instrument, drink!"
+    },{
+        header: "BOOP!",
+        body: "Touch a player's nose of your choice. That player must drink!"
+    },{
+        header: "Sharing Is Caring",
+        body: "Swap drinks with a player of your choice, you both drink!"
+    },{
+        header: "Love Thy Neighbor",
+        body: "Hold hands with the player to your right until your next turn."
+    },{
+        header: "Take One For The Team",
+        body: "All players vote on a liquor. Take a shot of it."
+    },{
+        header: "Let Me Get That For You",
+        body: "The next player that has to drink is not allowed to touch their cup. You must pour the drink in their mouth."
+    },{
+        header: "Show Me What You Got",
+        body: "All players must show off their tattoos and take a drink. If any player is unwilling to show one, they must take a shot instead."
+    },{
+        header: "Witty Title Goes Here",
+        body: "The next player to swear takes a shot!"
+    },{
+        header: "The Card Of Doom",
+        body: "Finish your drink, then take a shot!"
+    },{
+        header: "Bros Before Hoes",
+        body: "All males must drink!"
+    },{
+        header: "Chicks Before Dicks",
+        body: "All females must drink!"
+    },{
+        header: "Communion",
+        body: "All religious players must drink!"
+    },{
+        header: "An Apple A Day",
+        body: "All players who own an iPhone must drink!"
+    },{
+        header: "Do Androids Dream Of Electric Sheep?",
+        body: "All players who own an Android phone must drink!"
+    },{
+        header: "Prime Time",
+        body: "All players must reveal their latest Amazon purchase. The player with the most expensive item must drink!"
+    },{
+        header: "Popularity Contest",
+        body: "The player with the most friends on Facebook must take a shot!"
+    },{
+        header: "Show Me The Money!",
+        body: "All players must produce physical currency that they own. Any player who cannot must drink!"
+    },{
+        header: "Get Down Mr. President!",
+        body: "The next time a player has to take a shot, you take it for them!"
+    },{
+        header: "",
+        body: ""
+    },{
+        header: "",
+        body: ""
+    },{
+        header: "",
+        body: ""
+    },{
+        header: "",
+        body: ""
+    },{
+        header: "",
+        body: ""
+    },{
+        header: "",
+        body: ""
+    },{
+        header: "",
+        body: ""
+    },{
+        header: "",
+        body: ""
+    },{
+        header: "",
+        body: ""
+    },{
+        header: "",
+        body: ""
+    },{
+        header: "",
+        body: ""
+    },{
+        header: "",
+        body: ""
+    }
+*/
+]
--- game/assets/js/code.js
+++ game/assets/js/code.js
@@ -1,3 +1,20 @@
+/*
+TODO:
+build modal to get # of players and their names
+restructure decay to be turns = rounds * players
+rebuild Ongoing Effects table each turn
+*/
+
+
+rules = [];
+players = 0;
+currentPlayer = "Corey";
+i = 0;
+ruleCount = 0;
+//cards = shuffle(cards); //commented out for decay testing
+card = Object.keys(cards);
+table = document.getElementById("ongoingtable");
+tbody = document.getElementById("ongoingtbody");
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
     // While there remain elements to shuffle...
@@ -12,27 +29,52 @@ function shuffle(array) {
     }
     return array;
 }
+function Rule(id, header, body, decay, player) {
+    this.id = id;
+    this.header = header;
+    this.body = body;
+    this.decay = decay;
+    this.player = player;
+}
+function Player(id, name) {
+    this.id = id;
+    this.name = name;
+}
+function newTurn() {
+    //decay loop
+    //destroy ongoing effects table
+    tbody.innerHTML = "";
+    for (loop = 0; loop < ruleCount; loop++){
+        if (rules[loop].decay > 0) {
+            tbody.innerHTML += "<tr><td>" + rules[loop].id + "</td><td>" + rules[loop].header + "</td><td>" + rules[loop].body + "</td><td>" + rules[loop].decay + "</td><td>" + rules[loop].player + "</td></tr>"; //add rule to Ongoing Effects table
+        }
+        rules[loop].decay--; //decriment existing rules with decay
+    }
+    //change active player
 
-var i = 0;
-cards = shuffle(cards);
-var card = Object.keys(cards);
-
+    //call showCard
+    showCard();
+}
 function showCard() {
-    if (i === card.length) { //shuffle deck and restart
+    if (i === card.length) { //if 0 cards left in deck, shuffle deck and restart
         i = 0;
         alert("No new cards to display. Shuffling deck and starting over.");
         document.getElementById('card').innerHTML = "<h6>Click the button below to begin</h6>";
-        cards = shuffle(cards);
+//        cards = shuffle(cards); //commented out for decay testing
     } else {
         var newCard = cards[card[i]];
-        if (newCard.header === "") { //ignore card if it's blank
+        if (newCard.header === "") { //if card is blank, ignore this card and move on to the next
             i++;
             showCard();
         } else { //display card
-        document.getElementById('card').innerHTML = '<h3>' + newCard.header + '</h3><br/><h6>' + newCard.body + '</h6>';
-        i++;
+            document.getElementById('card').innerHTML = '<h3>' + newCard.header + '</h3><br/><h6>' + newCard.body + '</h6>';
+            i++;
+            if (newCard.decay > 0) { //if card has a decay value...
+                rule = new Rule(ruleCount, newCard.header, newCard.body, newCard.decay, currentPlayer); //create new rule
+                rules.push(rule);
+                ruleCount++;
+            };
         };
     };
-};
-
-document.getElementById("nextCard").addEventListener('click', showCard);
+}
+document.getElementById("nextCard").addEventListener('click', newTurn);
